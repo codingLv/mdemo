7ed88ce781ae3eab9fe8f19452e18b5cda54c768
dezhonglv <891890272@qq.com>
2017-11-01 15:38:43 +0800

    maven集成jetty插件启动springmvc 进步+1

--- mdemo/src/main/java/com/cn/spring/mvc/test/HelloWorld.java
+++ mdemo/src/main/java/com/cn/spring/mvc/test/HelloWorld.java
@@ -1,7 +1,8 @@
-package test;
+package com.cn.spring.mvc.test;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -9,6 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class HelloWorld
  */
+@WebServlet("/HelloWorld")
 public class HelloWorld extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
